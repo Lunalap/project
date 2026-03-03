6fa63b7c4d7d97188ea636d70f7a8023f50ef253
luna <gkgk1114@gmail.com>
2026-03-03 16:59:30 +0900

    feat: add reviewer column with conditional rendering for assignment

--- luna/src/pages/invoice/colums.tsx
+++ luna/src/pages/invoice/colums.tsx
@@ -43,6 +43,48 @@ export const columns: ColumnDef<InvoiceEntry>[] = [
       </button>
     ),
   },
+
+  /*
+
+  {
+    accessorKey: "reviewer",
+    header: "Reviewer",
+    cell: ({ row }) => {
+      const isAssigned = row.original.reviewer !== "Assign reviewer"
+
+      if (isAssigned) {
+        return row.original.reviewer
+      }
+
+      return (
+        <>
+          <Label htmlFor={`${row.original.id}-reviewer`} className="sr-only">
+            Reviewer
+          </Label>
+          <Select>
+            <SelectTrigger
+              className="w-38 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate"
+              size="sm"
+              id={`${row.original.id}-reviewer`}
+            >
+              <SelectValue placeholder="Assign reviewer" />
+            </SelectTrigger>
+            <SelectContent align="end">
+              <SelectItem value="Eddie Lake">Eddie Lake</SelectItem>
+              <SelectItem value="Jamik Tashpulatov">
+                Jamik Tashpulatov
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </>
+      )
+    },
+  },
+
+  */ 
+
+
+
   {
     accessorKey: "department_name",
     header: ({ column }) => (
